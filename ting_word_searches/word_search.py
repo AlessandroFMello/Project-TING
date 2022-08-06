b318f01c08ab3177db00d69b89ce24f19a79cdd5
Alessandro Fuhr de Mello <ale_95_f.mello@hotmail.com>
2022-08-05 23:16:09 -0300

    feat: Implements search_by_word function in word_search.py

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -19,3 +19,21 @@ def exists_word(word, instance):
 
 def search_by_word(word, instance):
     """Aqui irá sua implementação"""
+    data_list = list()
+    occurrencies = list()
+    for queue_item in instance.data:
+        for index, linha in enumerate(queue_item['linhas_do_arquivo']):
+            if word.lower() in linha.lower():
+                occurrencies.append({
+                    'linha': index + 1,
+                    'conteudo': linha
+                    })
+
+        if len(occurrencies) > 0:
+            data_list.append({
+                'palavra': word,
+                'arquivo': queue_item['nome_do_arquivo'],
+                'ocorrencias': occurrencies
+            })
+
+    return data_list
