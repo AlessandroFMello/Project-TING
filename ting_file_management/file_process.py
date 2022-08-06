370151dc5e49b0c9cdfb6156e46f9074a87bda86
Alessandro Fuhr de Mello <ale_95_f.mello@hotmail.com>
2022-08-05 23:15:00 -0300

    feat: Implements process function in file_process.py

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,24 @@
+import sys
+from ting_file_management.file_management import txt_importer
+
+
 def process(path_file, instance):
     """Aqui irá sua implementação"""
+    for queue_item in instance.data:
+        if queue_item['nome_do_arquivo'] == path_file:
+            return f"Arquivo {path_file} já está na fila"
+
+    file_reader = txt_importer(path_file)
+
+    result = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(file_reader),
+        "linhas_do_arquivo": file_reader,
+    }
+
+    instance.enqueue(result)
+    print(result, file=sys.stdout)
+    return f"Arquivo {path_file} adicionado à fila"
 
 
 def remove(instance):
