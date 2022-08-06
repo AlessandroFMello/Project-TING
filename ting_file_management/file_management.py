b5a5463bfe3acdc94279108e769f3d7a73f8d65a
Alessandro Fuhr de Mello <ale_95_f.mello@hotmail.com>
2022-08-05 23:14:31 -0300

    feat: Implements txt_importer function in file_management.py

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,17 @@
+import sys
+
+
 def txt_importer(path_file):
     """Aqui irá sua implementação"""
+    try:
+        if path_file.endswith('.txt'):
+            with open(path_file, 'r') as file:
+                read_file = file.read().strip().split('\n')
+                return read_file
+        else:
+            print("Formato inválido", file=sys.stderr)
+    except FileNotFoundError:
+        print(
+            f"Arquivo {path_file} não encontrado",
+            file=sys.stderr
+            )
